f86716c4a12b880e7984546e5e5e09f89b42ae2d
Github7701 <mn8586229@gmail.com>
2024-09-13 13:25:08 +0600

    Update Joy25.js

--- scripts/commands/Joy25.js
+++ scripts/commands/Joy25.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "Joy25.js",
+	name: "Asif Ahmed.js",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😐")==0) {
+	if (body.indexOf("😭")==0 || body.indexOf("😭")==0 || body.indexOf("😭")==0 || body.indexOf("😐")==0) {
 		var msg = {
-				body: "আমি জানি  কিছু মানুষ আমাকে..!\n অবহেলা করার মধ্যে মানসিক শান্তি পায়🥀😅\n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "আমি জানি  কিছু মানুষ আমাকে..!\n অবহেলা করার মধ্যে মানসিক শান্তি পায়🥀😅\n\n𝐌𝐃 Asif Ahmed",
 				attachment: fs.createReadStream(__dirname + `/Joy/joy11.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
