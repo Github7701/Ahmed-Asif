50c17c38e08ecdb7dae3602f8682c202d5494cc3
Github7701 <mn8586229@gmail.com>
2024-09-13 13:21:32 +0600

    Update Joy24.js

--- scripts/commands/Joy24.js
+++ scripts/commands/Joy24.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 module.exports = {
   config:{
-	name: "Joy24",
+	name: "Anisur Rohman",
         version: "1.0.1",
         prefix: false,
 	permssion: 0,
@@ -16,9 +16,9 @@ handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
-	if (body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf(" ")==0 || body.indexOf("😴")==0) {
+	if (body.indexOf("babu")==0 || body.indexOf("babu")==0 || body.indexOf("babu")==0 || body.indexOf("😴")==0) {
 		var msg = {
-				body: "পৃথিবীতে প্রত্যেকটা মানুষের\nহৃদয়ে ব্যথা আছে\nশুধু প্রকাশ করার ধরন টা আলাদা….!.!\n\n𝐌𝐃 𝐉𝐔𝐁𝐀𝐄𝐃 𝐀𝐇𝐌𝐄𝐃 𝐉𝐎𝐘",
+				body: "পৃথিবীতে প্রত্যেকটা মানুষের\nহৃদয়ে ব্যথা আছে\nশুধু প্রকাশ করার ধরন টা আলাদা….!.!\n\n𝐌𝐃 Anisur Rohman",
 				attachment: fs.createReadStream(__dirname + `/Joy/efa3.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
